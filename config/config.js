ed1163281db145a3ca37db21ab766855356bf431
nayere <rebecca.nayere@gmail.com>
2024-07-09 22:41:25 +0300

    craete the db configuration

---
+++ config/config.js
@@ -0,0 +1,24 @@
+const { password, database, host } = require('pg/lib/defaults');
+
+require('dotenv').config();
+
+module.exports = {
+        development:{
+        username:process.env.DB_USER,
+        password:process.env.DB_PASS,
+        database:process.env.DB_NAME,
+        host:process.env.DB_HOST,
+        dialect: 'postgres'
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+      }
+};
+
+
+
+     
