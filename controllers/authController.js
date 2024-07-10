5bd4310f88249954f301d1c52d6eacb2392b6bc1
nayere <rebecca.nayere@gmail.com>
2024-07-10 09:38:14 +0300

    add login logic in auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,3 +21,30 @@ exports.register = async (req, res) => {
       res.status(500).json({ message: 'Internal server error' });
     }
   };  
+  exports.login = async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      
+      const user = await User.findOne({ where: { username } });
+      if (!user) {
+        return res.status(400).json({ message: 'Invalid username or password' });
+      }
+  
+     
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid username or password' });
+      }
+  
+    
+      const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+     
+      res.status(200).json({ token, user: { id: user.id, username: user.username } });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+  
