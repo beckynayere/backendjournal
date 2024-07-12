e49f1e6b26e87c788ef2f307ff150a5e89d187a0
nayere <rebecca.nayere@gmail.com>
2024-07-12 20:04:24 +0300

    create indexjs

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import sequelize from './models';
-import authRoutes from './routes/authRoutes';
-import userRoutes from './routes/userRoutes';
-import journalRoutes from './routes/journalEntryRoutes';
-import dotenv from 'dotenv';
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import bodyParser from "body-parser";
+import userRoutes from "./routes/userRoutes";
+import journalEntryRoutes from "./routes/journalEntryRoutes";
 
-dotenv.config();
+createConnection().then(() => {
+  const app = express();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+  app.use(bodyParser.json());
 
-app.use(cors());
-app.use(bodyParser.json());
+  app.use("/api/users", userRoutes);
+  app.use("/api/journal-entries", journalEntryRoutes);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/journals', journalRoutes);
-
-sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}).catch(err => {
-    console.error('Unable to connect to the database:', err);
-});
+  app.listen(3000, () => {
+    console.log("Server started on port 3000!");
+  });
+}).catch(error => console.log(error));
